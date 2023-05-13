c882670e0a996fcd0bbd05652f5c41a2f167f697
Romain Cambonie <romain.cambonie@gmail.com>
2023-05-13 12:52:30 +0200

    feat: initial commit

---
+++ repository.tf
@@ -0,0 +1,66 @@
+data "github_organization" "organization" {
+  name = var.github_organization
+}
+
+resource "github_repository" "repository" {
+  name                   = var.github_repository
+  description            = var.github_repository_description
+  visibility             = "public"
+  auto_init              = true
+  delete_branch_on_merge = true
+  topics                 = var.github_repository_topics
+}
+
+resource "github_branch_default" "main" {
+  repository = github_repository.repository.name
+  branch     = "main"
+
+  depends_on = [github_repository.repository]
+}
+
+resource "github_branch_protection" "main_branch_protection" {
+  repository_id                   = github_repository.repository.node_id
+  pattern                         = github_branch_default.main.branch
+  require_signed_commits          = true
+  required_linear_history         = true
+  require_conversation_resolution = true
+  allows_force_pushes             = var.allow_push_to_default_branch
+  allows_deletions                = false
+
+  required_pull_request_reviews {
+    required_approving_review_count = 1
+  }
+}
+
+locals {
+  allowed_branch_patterns = [
+    "build/*",
+    "chore/*",
+    "ci/*",
+    "docs/*",
+    "feat/*",
+    "fix/*",
+    "perf/*",
+    "refactor/*",
+    "revert/*",
+    "style/*",
+    "test/*",
+  ]
+}
+
+resource "github_branch_protection" "other_branches_protection" {
+  for_each = toset(local.allowed_branch_patterns)
+
+  repository_id                   = github_repository.repository.node_id
+  pattern                         = each.value
+  require_signed_commits          = true
+  required_linear_history         = true
+  require_conversation_resolution = true
+  allows_force_pushes             = true
+  allows_deletions                = true
+
+  required_status_checks {
+    strict   = true
+    contexts = var.required_context_checks
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,40 @@
+variable "github_organization" {
+  description = "The github organization name"
+  nullable    = false
+  default     = false
+}
+
+variable "github_repository" {
+  description = "The repository which host the code within the organization"
+  nullable    = false
+  default     = false
+}
+
+variable "github_repository_topics" {
+  type        = list(string)
+  description = "The topics present on the repository"
+  nullable    = true
+  default     = []
+}
+
+variable "allow_push_to_default_branch" {
+  type        = bool
+  description = "Allows to push directly on default branch, if false a PR is mandatory"
+  nullable    = true
+  default     = false
+}
+
+variable "required_context_checks" {
+  type        = list(string)
+  description = "Required CI checks to pass,following the format workflowName/jobName eg: Validate/Prettier"
+  nullable    = true
+  default     = []
+}
+
+variable "github_repository_description" {
+  type        = string
+  description = "Repository description"
+  nullable    = true
+  default     = "An awesome repository"
+}
+
